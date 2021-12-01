1597fed9d7e1c63bc356013214a37a8b9ac155b7
nett_hier <nett_hier>
2021-12-01 12:33:27 +0100

    2021 Day 1

---
+++ aoc-2021/src/bin/day1.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let input = include_str!("../../inputs/day1");
+    let nums: Vec<u32> = input.lines().map(|e| e.parse::<u32>().unwrap()).collect();
+
+    let part_one = nums.windows(2).filter(|e| e[1] > e[0]).count();
+    println!("Part 1: {}", part_one);
+
+    let part_two = nums.windows(4).filter(|e| e[3] > e[0]).count();
+    println!("Part 2: {}", part_two);
+}
