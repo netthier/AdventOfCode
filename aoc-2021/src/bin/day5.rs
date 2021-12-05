f308b010221f9799d54724256ac99515028f62d1
nett_hier <nett_hier>
2021-12-05 07:16:36 +0100

    Hacky optimization

--- aoc-2021/src/bin/day5.rs
+++ aoc-2021/src/bin/day5.rs
@@ -14,13 +14,6 @@ fn main() {
         .collect();
 
     let mut field = [[0; 1000]; 1000];
-    let counter = |field: &[[u32; 1000]]| {
-        field
-            .iter()
-            .map(|e| e.iter().filter(|e| e > &&1))
-            .flatten()
-            .count()
-    };
 
     for line in parsed.iter() {
         if line[0] == line[2] {
@@ -31,23 +24,35 @@ fn main() {
             for x in min(line[0], line[2])..=max(line[0], line[2]) {
                 field[x][line[1]] += 1;
             }
-        }
-    }
-
-    println!("Part 1: {}", counter(&field));
-
-    for line in parsed.iter().filter(|e| e[0] != e[2] && e[1] != e[3]) {
-        let sign = if line[1] < line[3] { 1 } else { -1 };
-        if line[0] < line[2] {
-            for (idx, x) in (line[0]..=line[2]).enumerate() {
-                field[x][((line[1] as i32) + (idx as i32) * sign) as usize] += 1;
-            }
         } else {
-            for (idx, x) in (line[2]..=line[0]).enumerate() {
-                field[x][((line[3] as i32) + (idx as i32) * -sign) as usize] += 1;
+            let sign = if line[1] < line[3] { 1 } else { -1 };
+            if line[0] < line[2] {
+                for (idx, x) in (line[0]..=line[2]).enumerate() {
+                    field[x][((line[1] as i32) + (idx as i32) * sign) as usize] += 1028;
+                }
+            } else {
+                for (idx, x) in (line[2]..=line[0]).enumerate() {
+                    field[x][((line[3] as i32) + (idx as i32) * -sign) as usize] += 1028;
+                }
             }
         }
     }
 
-    println!("Part 2: {}", counter(&field));
+    println!(
+        "Part 1: {}",
+        field
+            .iter()
+            .map(|e| e.iter().filter(|e| **e % 1028 > 1))
+            .flatten()
+            .count()
+    );
+
+    println!(
+        "Part 2: {}",
+        field
+            .iter()
+            .map(|e| e.iter().filter(|e| **e % 1028 + **e / 1028 > 1))
+            .flatten()
+            .count()
+    );
 }
