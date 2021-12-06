d8a5374c79148614d436083a18aff8800909ef22
nett_hier <nett_hier>
2021-12-06 08:21:37 +0100

    Clean up code

--- aoc-2021/rust/src/bin/day6.rs
+++ aoc-2021/rust/src/bin/day6.rs
@@ -1,26 +1,23 @@
 fn main() {
     let input = include_str!("../../../inputs/day6");
-    let mut nums: [u128; 7] = input
+    let mut nums: [[u64; 7]; 2] = input
         .trim()
         .split(',')
         .map(|e| e.parse::<usize>().unwrap())
-        .fold([0; 7], |mut acc, e| {
-            acc[e] += 1;
+        .fold([[0; 7]; 2], |mut acc, e| {
+            acc[0][e] += 1;
             acc
         });
 
     for i in 0..256 {
         let new_idx = (i + 2) % 7;
-        nums[new_idx] += nums[new_idx] >> 64;
-        nums[new_idx] &= u128::MAX >> 64;
-        nums[new_idx] |= nums[i % 7] << 64;
+        nums[0][new_idx] += nums[1][new_idx];
+        nums[1][new_idx] = nums[0][i % 7];
         if i == 79 || i == 255 {
             println!(
                 "Part {}: {}",
                 i / 255 + 1,
-                nums.iter()
-                    .map(|e| (e & u128::MAX >> 64) + (e >> 64))
-                    .sum::<u128>()
+                nums.iter().flatten().sum::<u64>()
             );
         }
     }
